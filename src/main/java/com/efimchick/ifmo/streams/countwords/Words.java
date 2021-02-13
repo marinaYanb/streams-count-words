80060a8442b4002a625935d37b40c3ec972170e6
Marina Ianborisova <egorovaur@mail.ru>
2021-02-14 01:08:51 +0300

    fixed method

--- src/main/java/com/efimchick/ifmo/streams/countwords/Words.java
+++ src/main/java/com/efimchick/ifmo/streams/countwords/Words.java
@@ -13,12 +13,14 @@ public class Words {
                 .flatMap(line -> Arrays.stream(line.trim().split(" ")))
                 .filter(word -> word.length() >= 4)
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
-                .entrySet().stream().filter(i -> i.getValue() >= 10).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-        Map<String, Long> resultMap = wordMap.entrySet().stream()
+                .entrySet().stream().filter(i -> i.getValue() >= 10)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue))
+                .entrySet().stream()
                 .sorted(Map.Entry.comparingByKey()).sorted(Map.Entry.comparingByValue(Collections.reverseOrder()))
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
                         (oldValue, newValue) -> oldValue, LinkedHashMap::new));
-        return resultMap.entrySet()
+
+        return wordMap.entrySet()
                 .stream()
                 .map(entry -> entry.getKey() + " - " + entry.getValue())
                 .collect(Collectors.joining("\n"));
