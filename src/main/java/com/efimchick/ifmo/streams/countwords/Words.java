2cb409b241698c8299a809d67845e6ea05b8b582
Marina Ianborisova <egorovaur@mail.ru>
2021-02-13 23:17:57 +0300

    implemented streamed method

--- src/main/java/com/efimchick/ifmo/streams/countwords/Words.java
+++ src/main/java/com/efimchick/ifmo/streams/countwords/Words.java
@@ -1,12 +1,26 @@
 package com.efimchick.ifmo.streams.countwords;
 
-
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Words {
 
     public String countWords(List<String> lines) {
-
-        return null;
+        Map<String, Long> wordMap = lines
+                .stream()
+                .map(word -> word.replaceAll("[^a-zA-Zа-яА-Я]", " ").toLowerCase().trim())
+                .flatMap(line -> Arrays.stream(line.trim().split(" ")))
+                .filter(word -> word.length() >= 4)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream().filter(i -> i.getValue() >= 10).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        Map<String, Long> resultMap = wordMap.entrySet().stream()
+                .sorted(Map.Entry.comparingByKey()).sorted(Map.Entry.comparingByValue(Collections.reverseOrder()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+        return resultMap.entrySet()
+                .stream()
+                .map(entry -> entry.getKey() + " - " + entry.getValue())
+                .collect(Collectors.joining("\n"));
     }
 }
