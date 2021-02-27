e072108dd490da8ba71fafd9568afdaa088308e3
Marina Ianborisova <egorovaur@mail.ru>
2021-02-27 21:56:11 +0300

    updated method

--- src/main/java/com/efimchick/ifmo/streams/countwords/Words.java
+++ src/main/java/com/efimchick/ifmo/streams/countwords/Words.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 public class Words {
 
     public String countWords(List<String> lines) {
-        Map<String, Long> wordMap = lines
+        return lines
                 .stream()
                 .map(word -> word.replaceAll("[^a-zA-Zа-яА-Я]", " ").toLowerCase().trim())
                 .flatMap(line -> Arrays.stream(line.trim().split(" ")))
@@ -16,11 +16,6 @@ public class Words {
                 .entrySet().stream().filter(i -> i.getValue() >= 10)
                 .sorted(Map.Entry.comparingByKey())
                 .sorted(Map.Entry.comparingByValue(Collections.reverseOrder()))
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
-                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
-
-        return wordMap.entrySet()
-                .stream()
                 .map(entry -> entry.getKey() + " - " + entry.getValue())
                 .collect(Collectors.joining("\n"));
     }
